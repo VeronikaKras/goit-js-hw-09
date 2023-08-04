4f55c195e339da635d4fd133f77f63d251285182
VeronikaKras <127940482+VeronikaKras@users.noreply.github.com>
2023-08-04 10:10:36 +0000

    Deploying to gh-pages from @ VeronikaKras/goit-js-hw-09@00938208139605d2b7c2148c4f0f8b4fc5c97ff1 🚀

---
+++ 01-color-switcher.4e7df1c6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".start");document.querySelector(".stop");t.addEventListener("click",(function(){setInterval((function(){"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.4e7df1c6.js.map
